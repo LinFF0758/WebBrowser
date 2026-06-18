88f95c05ef4a3f8cab89ba67595c05cdf14461ec
LinFF0758 <ken.lin@clvtk.com>
2026-06-18 15:09:34 +0800

    fix: gradlew DEFAULT_JVM_OPTS quotes causing JVM startup failure

--- WebBrowserApp/gradlew.bat
+++ WebBrowserApp/gradlew.bat
@@ -14,6 +14,8 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
+
 @rem Execute Gradle
 "%JAVA_HOME%/bin/java.exe" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
